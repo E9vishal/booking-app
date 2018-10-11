ae449bc39681606c0f8d27724ca2d201a2631bf3
Vishal Biradar <vishalbiradar205@gmail.com>
2018-10-11 17:04:03 +0530

    added first_name and last_name to devise signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  enum role: {Guest: 0, Client: 1, Manager: 2, Super_Manager: 3} 
 end
